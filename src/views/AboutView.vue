6769b5d765c4b39b7e7e591ef4988b34e1d5e5f3
Lucas Costa <lucas.costa@simova.com.br>
2025-05-14 20:44:40 -0300

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,24 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+</script>
+<style scoped>
+
+/* Pequenos dispositivos (celulares maiores e tablets, entre 481px e 767px) */
+@media (min-width: 481px) {
+
+}
+/* Dispositivos médios (tablets em retrato, entre 768px e 1023px) */
+@media (min-width: 768px) {
+
+}
+/* Dispositivos grandes (tablets em paisagem / laptops pequenos) */
+@media (min-width: 1024px) {
+
+}
+/* Telas grandes (desktops, TVs etc.) */
+@media (min-width: 1280px) {
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
   name: 'HomeView',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+});
 </script>
+<style scoped>
+
+/* Pequenos dispositivos (celulares maiores e tablets, entre 481px e 767px) */
+@media (min-width: 481px) {
+
+}
+/* Dispositivos médios (tablets em retrato, entre 768px e 1023px) */
+@media (min-width: 768px) {
+
+}
+/* Dispositivos grandes (tablets em paisagem / laptops pequenos) */
+@media (min-width: 1024px) {
+
+}
+/* Telas grandes (desktops, TVs etc.) */
+@media (min-width: 1280px) {
+
+}
+</style>
