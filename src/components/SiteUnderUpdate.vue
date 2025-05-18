d635f2fabc60c3508c01951fc4e115f4c24efc8f
Lucas Costa <lucas.costa@simova.com.br>
2025-05-18 11:19:21 -0300

    feat: adiciona links para os sites dos empreendimentos da empresa na tela de 'Site em atualização'

--- src/components/SiteUnderUpdate.vue
+++ src/components/SiteUnderUpdate.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="pt-16">
+  <v-container>
     <v-row>
       <v-col col="12">
         <v-img
@@ -9,41 +9,60 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col col="12" class="update-title">
+      <v-col col="12" class="update-title pb-0">
         <p>
           Site em Atualização!
         </p>
       </v-col>
     </v-row>
     <v-row class="d-flex justify-center">
-      <v-col col="12" class="update-subtitle">
+      <v-col col="12" class="update-subtitle pt-0">
         <p>
           Estamos trabalhando em um espaço novo, moderno e elegante para melhor te atender.
         </p>
       </v-col>
     </v-row>
     <v-row>
-      <v-col col="12" class="d-flex justify-center mb-8">
+      <v-col col="12" class="d-flex justify-center">
         <v-img 
           :src="excavatorGif"
           class="update-gif"
         />
       </v-col>
     </v-row>
+    <v-row class="d-flex flex-column align-center">
+      <v-col col="12" class="d-flex justify-center venture-subtitle pb-0">
+        <p>
+          Conheça nossos empreedimentos
+        </p>
+      </v-col>
+      <v-col col="12" class="d-flex justify-center text-center pt-0">
+        <v-list class="d-flex flex-row">
+          <v-list-item
+            v-for="venture in ventures"
+            :key="venture.image"
+          >
+            <a :href="venture.link" target="_blank">
+              <img class="venture-logo" :src="venture.image" :title="venture.title" :alt="venture.alt"/>
+            </a>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
     <v-row class="d-flex flex-column">
-      <v-col col="12" class="d-flex justify-center">
+      <v-col col="12" class="d-flex justify-center pb-0">
         <span class="follow-midia">
           Siga para saber mais
         </span>
       </v-col>
-      <v-col col="12" class="d-flex justify-center text-center">
+      <v-col col="12" class="d-flex justify-center text-center pt-0">
         <v-list class="d-flex flex-row">
           <v-list-item
             v-for="social in socialIcons"
             :key="social.icon"
           >
             <a :href="social.link" target="_blank">
-              <v-icon size="40" :icon="social.icon" color="black"/>
+              <v-icon size="36" :icon="social.icon" color="black"/>
             </a>
           </v-list-item>
         </v-list>
@@ -74,7 +93,20 @@ export default {
         link: 'https://wa.me/+557199999999999'
       }
     ],
-
+    ventures: [
+      {
+        image: require('@/assets/logo/logo-ilheus-select.png'),
+        alt: 'Logo do empreendimento Ilheus Select',
+        title: 'Ilheus Select',
+        link: 'https://www.ilheusselect.com.br/'
+      },
+      {
+        image: require('@/assets/logo/logo-mamanuca-vert.png'),
+        alt: 'Logo do empreendimento Mamanuca Praia do Forte',
+        title: 'Mamanuca Praia do Forte',
+        link: 'https://vivermamanuca.com.br/'
+      }
+    ],
   }),
 }
 </script>
@@ -88,16 +120,26 @@ export default {
   text-align: center;
   text-transform: uppercase;
   text-shadow: lightgray 1px 1px 3px;
-  color: rgb(90, 90, 90);
+  color: rgb(113, 1, 1);
 }
 .update-subtitle {
   font-size: 0.8rem;
   font-weight: 500;
   text-align: center;
   color: gray;
+  max-width: 300px;
 }
 .update-gif {
   height: 7rem;
+}
+.venture-logo {
+  height: 6rem;
+}
+.venture-subtitle {
+  font-size: 1rem;
+  font-weight: 600;
+  text-align: center;
+  color: rgb(60, 60, 60);
 }
 .follow-midia {
   font-size: 1rem;
@@ -117,9 +159,6 @@ export default {
   }
   .update-title {
     font-size: 1.2em;
-  }
-  .update-subtitle {
-    max-width: 450px;
   }
   .update-gif {
     height: 8rem;
