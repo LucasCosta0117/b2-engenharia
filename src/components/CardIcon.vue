7cef626b35779f4ca084f33cfb74217c8ee56ced
Lucas Costa <lucas.costa@simova.com.br>
2025-07-11 23:12:52 -0300

    feat: monta estrutura inicial da 'AboutView'

---
+++ src/components/CardIcon.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-card
+    class="mx-auto card-item-container"
+    hover
+  >
+    <v-icon class="card-item-icon" :icon="cardIcon" />
+    <v-card-item>
+      <v-card-title class="card-item-title">
+        {{ cardTitle }}
+      </v-card-title>
+    </v-card-item>
+    <v-card-text class="card-item-text">
+      {{ cardText }}
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+/**
+ * Cria um card contendo apenas um título e um texto
+ */
+export default {
+  name: 'CardItem',
+  props: {
+    /**
+     * Título a ser exibido do Card
+     */
+    cardTitle: {
+      type: String,
+    },
+    /**
+     * Texto a ser exebido no Card
+     */
+    cardText: {
+      type: String,
+    },
+    /**
+     * Texto a ser exebido no Card
+     */
+    cardIcon: {
+      type: String,
+    }
+  }
+}
+</script>
+<style scoped>
+.card-item-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 18rem;
+  padding: 1rem;
+}
+.card-item-icon {
+  margin-top: 1rem;
+  font-size: 4rem;
+  color: rgb(130,10,10);
+}
+.card-item-title {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+.card-item-text {
+  text-align: center;
+}
+
+/**
+ * Pequenos dispositivos
+ * Vuetify 'sm' Break Point
+ */
+@media (min-width: 600px) {
+
+}
+/**
+ * Dispositivos médios
+ * Vuetify 'md' Break Point
+ */
+@media (min-width: 960px) {
+
+}
+/**
+ * Dispositivos grandes
+ * Vuetify 'lg' Break Point
+ */
+@media (min-width: 1400px) {
+
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -15,16 +15,26 @@
           entregas sólidas, inovadoras e com alto padrão de excelência.
         </p>
       </div>
+      <div>"Imagens de projetos B2"</div>
+    </div>
+    <div class="section-about-p2">
+      <SectionTitle class="section-title" :main-title="'Nossos Valores'"
+        :sub-title="''" />
+      <div class="cards-business-values">
+        <CardIcon v-for="val in listBusinessValues" :key="val.title" :card-title="val.title" :card-text="val.text" :card-icon="val.icon"/>
+      </div>
     </div>
   </v-container>
 </template>
 <script>
+import CardIcon from '@/components/CardIcon.vue';
 import HeaderPage from '@/components/HeaderPage.vue';
 import SectionTitle from '@/components/SectionTitle.vue';
 
 export default {
   name: 'AboutView',
   components: {
+    CardIcon,
     HeaderPage,
     SectionTitle
   },
@@ -35,10 +45,47 @@ export default {
       mainTitle: 'Quem Somos',
       subTitle: 'Transformamos projetos em realidade'
     },
+    listBusinessValues: [
+      {
+        title: 'Dedicação Plena',
+        text: 'Entendemos a dedicação como uma demonstração de humildade, disponibilidade e pró-atividade nas relações de trabalho. Através dela, assumimos a condição de agentes na conclusão dos objetivos que nos propomos a alcançar.',
+        icon: 'mdi-hand-heart-outline'
+      },
+      {
+        title: 'Respeito às pessoas',
+        text: 'Respeito à vida e a todas as formas de diversidade. Dentro e fora do ambiente de trabalho, o respeito gera uma atmosfera que propicia o bem estar das pessoas necessário para o bem estar de nossas funções.',
+        icon: 'mdi-account-group'
+      },
+      {
+        title: 'Integridade',
+        text: 'Somente através da Integridade, sempre galgada pela ética e pela honestidade, será possível criar os verdadeiros laços de confiança e parceria.',
+        icon: 'mdi-scale-balance'
+      },
+      {
+        title: 'Responsabilidade',
+        text: 'A responsabilidade é o que nos mantém ativos para “fazer acontecer” os compromissos assumidos.',
+        icon: 'mdi-shield-account'
+      },
+      {
+        title: 'Melhoria Contínua',
+        text: 'A busca incessante pelo aperfeiçoamento dos processos.',
+        icon: 'mdi-trending-up'
+      },
+      {
+        title: 'Sustentabilidade',
+        text: 'Conciliação entre proteção ambiental e atividades econômicas e sociais, respeito à biodiversidade e Consumo consciente.',
+        icon: 'mdi-bee-flower'
+      },
+    ]
   }),
 }
 </script>
 <style scoped>
+.about-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
 .section-about-p1 {
   display: flex;
   flex-direction: column;
@@ -55,6 +102,16 @@ export default {
   text-align: justify;
   text-indent: 1rem;
   margin: 0rem 1rem;
+}
+.section-about-p2 {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+.cards-business-values {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
 /**
