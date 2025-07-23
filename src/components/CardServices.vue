0bee83009b3cdd8b932858ab914528878a10d55c
Lucas Costa <lucas.costa@simova.com.br>
2025-07-22 22:32:21 -0300

    feat: adiciona conteúdo a seção de Serviços

---
+++ src/components/CardServices.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-card class="card-services-container">
+    <div class="card-services-text">
+      <h3>{{ title }}</h3>
+      <p class="section-paragraph">
+        {{ paragraph }}
+      </p>
+    </div>
+    <div class="card-services-img" :style="{ backgroundImage: `url(${require(`@/assets/image/${imageName}`)})` }">
+    </div>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'CardServices',
+  props: {
+    /**
+     * Título para o card.
+     */
+    title: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Texto de descrição para o card.
+     */
+    paragraph: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Nome da imagem (salva em src/assets/image) para uso no background do card.
+     */
+    imageName: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+<style scoped>
+.card-services-container {
+  display: flex;
+  flex-direction: column;
+}
+.card-services-text {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+}
+.section-paragraph {
+  font-size: 1rem;
+}
+.card-services-img {
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  max-width: 100%;
+  height: 12rem;
+}
+
+/**
+ * Pequenos dispositivos
+ * Vuetify 'sm' Break Point
+ */
+@media (min-width: 600px) {
+
+}
+/**
+ * Dispositivos médios
+ * Vuetify 'md' Break Point
+ */
+@media (min-width: 960px) {
+
+}
+
+/**
+ * Dispositivos grandes
+ * Vuetify 'lg' Break Point
+ */
+@media (min-width: 1400px) {
+
+}
+</style>
--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -3,25 +3,108 @@
     :header-image="headerImage"
     :header-title="headerTitle"
   />
-  <v-container class="services-container">
-    <SiteUnderUpdate></SiteUnderUpdate>
-  </v-container>
+  <div class="services-container">
+    <CardServices
+      v-for="serv of listServices" :key="serv.title"
+      :title="serv.title"
+      :paragraph="serv.paragraph"
+      :imageName="serv.imageName"
+    />
+  </div>
 </template>
 <script>
-import SiteUnderUpdate from '@/components/SiteUnderUpdate.vue';
+import CardServices from '@/components/CardServices.vue';
 import HeaderPage from '@/components/HeaderPage.vue';
 
 export default {
   name: 'ServicesView',
   components: {
-    SiteUnderUpdate,
+    CardServices,
     HeaderPage
   },
   data: () => ({
     headerImage: require("@/assets/image/services-header.webp"),
-    headerTitle: 'Nossos serviços'
+    headerTitle: 'Nossos serviços',
+    listServices: [
+      {
+        title: 'Loteamento aberto e fechado',
+        paragraph: 'Planejamento, aprovação e execução de loteamentos com foco em mobilidade, segurança e infraestrutura de qualidade.',
+        imageName: 'compliance.webp'
+      },
+      {
+        title: 'Desenvolvimento Imobiliário',
+        paragraph: 'Gestão completa de empreendimentos, desde a viabilidade até a comercialização, com análise estratégica do mercado.',
+        imageName: 'compliance.webp'
+      },
+      {
+        title: 'Estruturação de Operações Financeiras',
+        paragraph: 'Captação e estruturação de recursos financeiros para viabilização e alavancagem de projetos imobiliários e de infraestrutura.',
+        imageName: 'compliance.webp'
+      },
+      {
+        title: 'Edificações horizontais e verticais',
+        paragraph: 'Construção de residenciais, comerciais e corporativos com soluções técnicas eficientes e alto padrão construtivo.',
+        imageName: 'compliance.webp'
+      },
+      {
+        title: 'Usinas Solares',
+        paragraph: 'Implantação de usinas fotovoltaicas com foco em eficiência energética e retorno sustentável para investidores e parceiros.',
+        imageName: 'compliance.webp'
+      },
+      {
+        title: 'Retrofit de Edificações',
+        text: 'Requalificação de imóveis com foco na valorização patrimonial, eficiência operacional e adequação às normas atuais.',
+        imageName: 'compliance.webp'
+      },
+      {
+        title: 'Infraestrutura',
+        paragraph: 'Execução de obras de infraestrutura urbana e industrial, com foco em durabilidade, funcionalidade e responsabilidade ambiental.',
+        imageName: 'compliance.webp'
+      },
+      {
+        title: 'Galpões Logísticos',
+        paragraph: 'Projetos e construção de centros logísticos otimizados, com alto desempenho operacional e adaptabilidade às demandas do setor.',
+        imageName: 'compliance.webp'
+      }
+    ]
   }),
 }
 </script>
 <style scoped>
+.services-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin: 4rem 1rem;
+}
+
+/**
+ * Pequenos dispositivos
+ * Vuetify 'sm' Break Point
+ */
+@media (min-width: 600px) {
+
+}
+/**
+ * Dispositivos médios
+ * Vuetify 'md' Break Point
+ */
+@media (min-width: 960px) {
+  .services-container {
+    max-width: 1000px;
+  }
+  .section-paragraph {
+    font-size: 1.1rem;
+  }
+}
+
+/**
+ * Dispositivos grandes
+ * Vuetify 'lg' Break Point
+ */
+@media (min-width: 1400px) {
+  .services-container {
+    max-width: 1200px;
+  }
+}
 </style>
