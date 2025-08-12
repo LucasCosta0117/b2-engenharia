7ff70b43f4b30ef782ab1bb15806c926309063f0
Lucas Costa <lucas.costa@simova.com.br>
2025-08-11 22:59:20 -0300

    refactor: torna dinâmico as props do CardGallery e altera o estilo do componente

--- src/components/CardGallery.vue
+++ src/components/CardGallery.vue
@@ -7,27 +7,33 @@
       <div class="card-thumb">
         <v-img cover class="card-thumb-img" :src="thumbUrl"/>
       </div>
-      <div class="card-features">
-        <div>
-          <div class="feature-item">
-            <v-icon size="18" icon="mdi-bed-king"></v-icon>
-            {{ feature.bedrooms }}
+      <div>
+        <div class="card-features">
+          <div>
+            <div class="feature-item">
+              <v-icon size="18" icon="mdi-bed-king"></v-icon>
+              {{ feature.bedrooms }}
+            </div>
+            <div class="feature-item">
+              <v-icon size="18" icon="mdi-ruler-square"></v-icon>
+              {{ feature.area }}
+            </div>
           </div>
-          <div class="feature-item">
-            <v-icon size="18" icon="mdi-ruler-square"></v-icon>
-            {{ feature.area }}
+          <div>
+            <div class="feature-item">
+              <v-icon size="18" icon="mdi-map-marker"></v-icon>
+              {{ feature.city }}
+            </div>
+            <div class="feature-item">
+              <v-icon size="18" icon="mdi-calendar-month"></v-icon>
+              {{ feature.launch }}
+            </div>
           </div>
         </div>
-        <div>
-          <div class="feature-item">
-            <v-icon size="18" icon="mdi-map-marker"></v-icon>
-            {{ feature.city }}
-          </div>
-          <div class="feature-item">
-            <v-icon size="18" icon="mdi-calendar-month"></v-icon>
-            {{ feature.launch }}
-          </div>
-        </div>
+        <a v-if="website" class="card-link" :href="website" target="_blank">
+          <span>Visite o site</span>
+          <i class="mdi mdi-page-last"></i>
+        </a>
       </div>
     </div>
   </div>
@@ -38,23 +44,55 @@
  */
 export default {
   name: 'CardGallery',
-  data() {
-    return {
-      title: 'Mamanuca Praia do Forte',
-      thumbUrl: require('@/assets/image/gallery-header.webp'),
-      feature: {
-        bedrooms: '4 quartos',
-        area: '45m²',
-        city: 'Praia do Forte - BA',
-        launch: 'jun/2025'
+  props: {
+    /**
+     * Nome do empreendimento
+     */
+    title: {
+      type: String,
+      require: true,
+      default: 'Employment'
+    },
+    /**
+     * Foto da capa do card
+     */
+    thumbUrl: {
+      type: String,
+      require: true,
+      default: require('@/assets/image/gallery-header.webp')
+    },
+    /**
+     * Características do empreendimento
+     */
+    feature: {
+      type: Object,
+      require: true,
+      default: () => {
+        return {
+          bedrooms: '2 quartos',
+          area: '50m²',
+          city: 'Salvador - BA',
+          launch: '2025'
+        }
       }
+    },
+    /**
+     * Acesso ao website do empreendimento
+     */
+    website: {
+      type: String,
+      require: false
     }
+  },
+  data() {
+    return {}
   }
 }
 </script>
 <style scoped>
 .card-container {
   display: flex;
+  width: 100%;
   height: 25rem;
   border-radius: 1rem;
 }
@@ -62,33 +100,33 @@ export default {
   display: flex;
   justify-content: start;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.4);
   color: white;
   font-weight: 400;
   font-size: 1.25rem;
   text-transform: uppercase;
   writing-mode: sideways-lr;
-  padding: 2rem;
+  padding: 1rem;
   margin-bottom: 1rem;
-  width: 20%;
+  width: 10%;
   transition: all 0.5s ease;
 }
 .card-container:hover .card-title {
-  background: linear-gradient(45deg, rgba(0, 0, 0, 1) 0%, rgba(110, 13, 13, 1) 100%);
+  background: rgba(110, 10, 10);
   margin-bottom: 0;
   font-weight: 600;
-  width: 15%;
+  width: 12%;
 }
 .card-info {
   display: flex;
   flex-direction: column;
   margin-bottom: 1rem;
-  width: 80%;
+  width: 90%;
   background: linear-gradient(75deg, rgb(255, 255, 255) 0%, rgb(226, 225, 225) 100%);
 }
 .card-container:hover .card-info {
   margin-bottom: 0;
-  width: 85%;
+  width: 88%;
 }
 .card-thumb {
   overflow: hidden;
@@ -112,14 +150,28 @@ export default {
   gap: 0.25rem;
   font-size: 0.8rem;
 }
+.card-link {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 1rem;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 0.85rem;
+  color:  rgba(110, 13, 13, 1);
+}
 
 /**
  * Pequenos dispositivos
  * Vuetify 'sm' Break Point
  */
 @media (min-width: 600px) {
-  .card-container {
-    width: 47.5%;
+  .card-features {
+    gap: 2rem;
+  }
+  .feature-item {
+    gap: 0.75rem;
   }
 }
 
@@ -128,8 +180,6 @@ export default {
  * Vuetify 'md' Break Point
  */
 @media (min-width: 960px) {
-  .card-container {
-    width: 30.5%;
-  }
+
 }
 </style>
